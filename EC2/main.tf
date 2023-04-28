87e7d388831df55c6ec7be1b3c2a2d6252013bb2
sdime401 <simon.dime31@gmail.com>
2023-04-27 22:05:06 -0400

    Modifying is my EC2/main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -12,11 +12,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "my-ec2" {
+resource "aws_instance" "my_ec2" {
 
   ami           = "ami-009c5f630e96948cb"
   instance_type = "t3.micro"
   key_name      = "awesome_key"
+
   tags = {
     "Name" = "my-ec2"
   }
